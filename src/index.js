0a63b58834b32a1efc8006ddb96e16c0020d08e6
finwo <finwo@pm.me>
2019-06-11 13:00:42 +0200

    <implicit> operator now supported; main returns factory instead now

--- src/index.js
+++ src/index.js
@@ -1,59 +1,66 @@
-const hasModule = require('./has-module'),
-      parser    = (function() {
-        if (hasModule('lucene')) return require('lucene');
-        if (hasModule('lucene-queryparser')) return require('lucene-queryparser');
-        if (hasModule('lucene-query-parser')) return require('lucene-query-parser');
-        throw new Error("None of 'lucene', 'lucene-queryparser' or 'lucene-query-parser' are installed");
-      })();
+// Factory
+const lucene = module.exports = function factory( parser ) {
 
-const lucene = module.exports = function compiler(query) {
+  // Parser must be given
+  if (!parser) {
+    throw new Error('No parser given. Must be one of \'lucene\',\'lucene-queryparser\',\'lucene-query-parser\'');
+  }
 
-  // Parse string query
-  if (!query) return () => 0;
-  if ('string' === typeof query) {
+  // The given parser must be compatible
+  if (('object' !== typeof parser) || ('function' !== typeof parser.parse)) {
+    throw new Error('Incompatible parser given. Must be one of \'lucene\',\'lucene-queryparser\',\'lucene-query-parser\'');
+  }
+
+  // Returns Function(Object):Number
+  function compile(query) {
+    if ('string' !== typeof query) return ()=>0;
+    if (!query) return ()=>0;
+
+    // Actually compile what was given
     try {
       query = parser.parse(query);
-    } catch (e) {
-      return () => 0;
+    } catch(e) {
+      return ()=>0;
     }
-  }
 
-  // Compile combined
-  if (query.operator) {
-    return lucene.operators[query.operator](
-      lucene(query.left),
-      lucene(query.right),
-    );
-  }
-
-  // Wrapped
-  if (query.left) {
-    return lucene(query.left);
-  }
-
-  // Ensure default boost
-  query.boost = query.boost || 1;
-
-  // Return the first detected filter
-  for (const filter of lucene.filters) {
-    if (filter.detect(query)) {
-      return filter.compile(query);
+    // Compile multi-query
+    if (query.operator) {
+      return lucene.operators[query.operator](
+        compile(query.left),
+        compile(query.right),
+      );
     }
+
+    // Wrapped
+    if (query.left) {
+      return compile(query.left);
+    }
+
+    // Ensure default boost
+    query.boost = query.boost || 1;
+
+    // Return the first detected filter
+    for (const filter of lucene.filters) {
+      if (filter.detect(query)) {
+        return filter.compile(query);
+      }
+    }
+
+    // Return no match
+    return ()=>0;
   }
 
-  // Return no match
-  return () => 0;
-};
-
-// Return the data when matching
-// TODO: lucene() = { f() passthrough: f() }
-lucene.passthrough = function(query) {
-  let match = lucene(query);
-  return function(data) {
-    if(match(data)) return data;
-    return undefined;
+  // Return the data when matching
+  compile.passthrough = function(parser, query) {
+    let match = compile(query);
+    return function(data) {
+      if(match(data)) return data;
+      return undefined;
+    };
   };
-};
+
+  return compile;
+}
 
 // Add filters & operators
 lucene.filters   = require('./filters');
--- src/operators/index.js
+++ src/operators/index.js
@@ -1,5 +1,6 @@
-const operators      = module.exports = {};
-operators['OR']      = require('./or');
-operators['AND']     = require('./and');
-operators['OR NOT']  = require('./or-not')(operators);
-operators['AND NOT'] = require('./and-not')(operators);
+const operators         = module.exports = {};
+operators['OR']         = require('./or');
+operators['AND']        = require('./and');
+operators['OR NOT']     = require('./or-not')(operators);
+operators['AND NOT']    = require('./and-not')(operators);
+operators['<implicit>'] = require('./and')(operators);
