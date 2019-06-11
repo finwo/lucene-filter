b82481c01ab05558d91c1187ec679e8d7db68970
Robin Bron <finwo@pm.me>
2019-06-11 14:10:15 +0200

    Minimal field and implicit tests

---
+++ src/implicit.test.js
@@ -0,0 +1,16 @@
+import expect from 'expect';
+const implicit = require('./implicit');
+
+test('Ensure basics', async () => {
+  expect(typeof implicit).toBe('function');
+});
+
+test('Verify loop', async () => {
+  let out = [];
+  implicit({foo:'bar',pizza:'calzone'}, arg => {
+    out.push(arg);
+  });
+
+  expect(out[0]).toBe('bar');
+  expect(out[1]).toBe('calzone');
+});
