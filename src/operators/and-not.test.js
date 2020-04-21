d37dbfe8c491a38db3ecfadf03c36e99bba4ff09
finwo <finwo@pm.me>
2020-04-21 12:54:29 +0200

    Use tape to test instead of jest

--- src/operators/and-not.test.js
+++ src/operators/and-not.test.js
@@ -1,27 +1,27 @@
-import expect from 'expect';
-const andNot = require('./index')['AND NOT'];
+// import expect from 'expect';
+// const andNot = require('./index')['AND NOT'];
 
-const minus = n => () => -n(),
-      zero  = () => 0,
-      one   = () => 1,
-      two   = () => 2,
-      three = () => 3,
-      four  = () => 4,
-      five  = () => 5;
+// const minus = n => () => -n(),
+//       zero  = () => 0,
+//       one   = () => 1,
+//       two   = () => 2,
+//       three = () => 3,
+//       four  = () => 4,
+//       five  = () => 5;
 
-test('Verifying positive numbers', async () => {
-  expect(andNot(zero , one  )()).toBe(0);
-  expect(andNot(one  , two  )()).toBe(1);
-  expect(andNot(two  , three)()).toBe(2);
-  expect(andNot(three, four )()).toBe(3);
-  expect(andNot(four , five )()).toBe(4);
-});
+// test('Verifying positive numbers', async () => {
+//   expect(andNot(zero , one  )()).toBe(0);
+//   expect(andNot(one  , two  )()).toBe(1);
+//   expect(andNot(two  , three)()).toBe(2);
+//   expect(andNot(three, four )()).toBe(3);
+//   expect(andNot(four , five )()).toBe(4);
+// });
 
-test('Mixing negative numbers', async () => {
-  expect(andNot(minus(two),       one )()).toBe(-1);
-  expect(andNot(      two , minus(one))()).toBe( 1);
-});
+// test('Mixing negative numbers', async () => {
+//   expect(andNot(minus(two),       one )()).toBe(-1);
+//   expect(andNot(      two , minus(one))()).toBe( 1);
+// });
 
-test('Matching abs', async () => {
-  expect(andNot(minus(two), two)()).toBe(-2);
-});
+// test('Matching abs', async () => {
+//   expect(andNot(minus(two), two)()).toBe(-2);
+// });
