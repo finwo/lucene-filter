25ab06f914187be1397ec9593931e19675f72cb8
paulcanning <pcanning@gmail.com>
2022-05-31 16:45:54 +0100

    Add new comparison filter to filter list

--- src/filters/index.js
+++ src/filters/index.js
@@ -2,6 +2,7 @@ const filters = module.exports = [];
 filters.push(require('./regexp'));
 filters.push(require('./number/range'));
 filters.push(require('./number/exact'));
+filters.push(require('./number/comparison'));
 filters.push(require('./string/starts-with'));
 filters.push(require('./string/ends-with'));
 filters.push(require('./string/contains'));
